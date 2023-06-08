e9b29d03ee1c6c071638f0589590be54e4578a83
Ramon Chavez <rachavez1974@gmail.com>
2023-06-08 12:21:52 -0400

    Finish repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+  let str = ""
 
+  if (repeat >= 0) {
+    for(let i = 0; i < repeat; i++){
+      str += string
+    }
+  } else {
+   return 'ERROR'
+  }
+  return str
 };
 
 // Do not edit below this line
