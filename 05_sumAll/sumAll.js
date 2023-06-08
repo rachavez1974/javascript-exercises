6fc455319880750c272b5448a41e1f3d6b6954ce
Ramon Chavez <rachavez1974@gmail.com>
2023-06-08 15:40:04 -0400

    Finish sumAll exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+  if (typeof(start) != "number" || typeof(finish) != "number") {
+    return 'ERROR'
+  } else if((start < 0 || finish < 0)){
+    return 'ERROR'
+  } else if (start > finish) {
+    let holdFinish = finish
+    finish = start
+    start = holdFinish
+  }
 
+  let sum = 0
+  for(let i = start; i <= finish; i++){
+    sum = sum + i
+  }
+  return sum
 };
 
 // Do not edit below this line
