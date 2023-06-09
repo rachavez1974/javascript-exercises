d6cf1ab8d0a7608af429d0c3a9918e6dc4c0ca0a
Ramon Chavez <rachavez1974@gmail.com>
2023-06-09 10:09:58 -0400

    Finsih calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let product = 1
+  for(let i = 0; i < args.length; i++){
+    product *= args[i]
+  }
+  return product
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return number ** power
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0 || number === 1) {
+    return 1
+  } else if (number === 2){
+    return 2
+  } else {
+    return number * factorial(number - 1)
+  }
 };
+
 
 // Do not edit below this line
 module.exports = {
