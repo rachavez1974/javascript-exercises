555917b69e33b37736e4decb27f3c24b9374c0a5
Ramon Chavez <rachavez1974@gmail.com>
2023-06-11 12:56:29 -0400

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const standarizeString = (string) => {
+  return string.match(/[A-Z|a-z|0-9]/g).join().toLowerCase()
+}
 
+const reversedString = (string) => {
+  newString = standarizeString(string)
+  let reverse = ''
+  for(let i = newString.length - 1; i >= 0; i--){
+    reverse += newString[i]
+  }
+  return reverse
+}
+
+const palindromes = function (string) {
+  if (standarizeString(string) === reversedString(string)){
+    return true
+  } else {
+    return false
+  }
 };
 
+console.log(standarizeString('Racecar!'))
+console.log(palindromes('Racecar!'))
 // Do not edit below this line
 module.exports = palindromes;
