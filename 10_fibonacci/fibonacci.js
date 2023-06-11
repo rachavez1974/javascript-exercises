18a211c386fcc9a158b068f4e2bec1e00a08261a
Ramon Chavez <rachavez1974@gmail.com>
2023-06-11 14:29:17 -0400

    Finish fibonacci series exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+// const
 
+const fibonacci = function(number) {
+  let fibonacciNumbers = []
+
+  if(number < 0){
+    return 'OOPS'
+  }else {
+    number = parseInt(number)
+  }
+
+  for(let i = 0; i < number; i++){
+    if(i === 0 || i === 1){
+      fibonacciNumbers.push(1)
+    }else{
+      fibonacciNumbers.push(fibonacciNumbers[i-1] + fibonacciNumbers[i-2])
+    }
+  }
+  return fibonacciNumbers[number - 1]
 };
 
 // Do not edit below this line
