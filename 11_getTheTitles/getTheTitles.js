3df6cf407db241e8541108a3af95fee0d3a3df87
Ramon Chavez <rachavez1974@gmail.com>
2023-06-11 14:41:37 -0400

    Finish getTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+  const title = (book) => {
+    return book.title
+  }
+
+  return books.map(title)
 };
 
 // Do not edit below this line
