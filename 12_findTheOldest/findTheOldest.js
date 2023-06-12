471580af50c700fd0eb29fe7b2ca90c00254ad3c
Ramon Chavez <rachavez1974@gmail.com>
2023-06-12 18:28:56 -0400

    Finish FindTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = age(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath)
+    const currentAge = age(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+    return oldestAge < currentAge ? currentPerson : oldestPerson
 
+  });
 };
 
+
+const age = (birth, death) => {
+  if(death === undefined){
+    death = new Date().getFullYear();
+  }
+  return death - birth
+}
 // Do not edit below this line
 module.exports = findTheOldest;
